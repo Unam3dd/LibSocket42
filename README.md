94e2a889f3e2082af7b9c5af5d97e239bf38dc1a
Samuel Tales <stales@3E7.42angouleme.fr>
2023-01-20 20:25:13 +0100

    update read

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # LibSocket42
-Simple Socket Library writed in C++98 for 42
+Simple Socket Library writed in C++98 for 42 [WebServ/IRC]
--- src/socket/read.cpp
+++ src/socket/read.cpp
@@ -6,7 +6,7 @@
 /*   By: stales <stales@student.42angouleme.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 17:28:29 by stales            #+#    #+#             */
-/*   Updated: 2023/01/20 18:18:57 by stales           ###   ########.fr       */
+/*   Updated: 2023/01/20 20:24:46 by stales           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,7 +57,7 @@ int Socket::Read(void *buf, size_t len)
 *********************************************************************/
 int	Socket::Read(std::string& buf, size_t len)
 {
-	buf.reserve(len);
+	if (buf.capacity() < len) buf.reserve(len);
 	return (read(this->_fd, (char *)buf.c_str(), buf.size()));
 }
 
@@ -73,6 +73,6 @@ int	Socket::Read(std::string& buf, size_t len)
 *********************************************************************/
 int	Socket::Read(int fd, std::string& buf, size_t len)
 {
-	buf.reserve(len);
+	if (buf.capacity() < len) buf.reserve(len);
 	return (read(fd, (char *)buf.c_str(), buf.size()));
 }
